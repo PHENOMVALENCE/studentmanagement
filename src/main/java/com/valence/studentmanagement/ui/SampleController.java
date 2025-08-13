406ee0be3e118e23741f82fb674a3a0d22e9733e
Sinare <Salum.Sinare@stanbic.co.tz>
2025-08-13 12:46:06 +0300

    Templates for front end

--- src/main/java/com/valence/studentmanagement/ui/SampleController.java
+++ src/main/java/com/valence/studentmanagement/ui/SampleController.java
@@ -31,6 +31,12 @@ public class SampleController {
 		return "list";
 	}
 	
+	@GetMapping("/message")
+	public String message(Model model) {
+		 
+		return "list";
+	}
+	
 	@PostMapping("/sample")
 	public String post(Model model,@ModelAttribute("studentInput") StudentInput studentInput,
 			RedirectAttributes ra) {
